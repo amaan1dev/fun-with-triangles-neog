6ca0b2482ff57d2801332609392c198fa733e7eb
Amaansyed309 <64450603+Amaansyed309@users.noreply.github.com>
2022-10-02 02:08:14 +0530

    update

--- quiz.js
+++ quiz.js
@@ -1,2 +1,21 @@
 const quizForm = document.querySelector(".quiz-form");
-const submitAnsBtn = document.querySelectorAll("#submit-btn");
+const submitAnsBtn = document.querySelector("#submit-btn");
+
+const outputText = document.querySelector("#output-text");
+
+const correctAnswers = ["30", "5", "equilateral", "250", "less than 90"];
+
+ function calculateScore(){
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    outputText.innerText = "Your score is " + score;
+ } 
+
+ submitAnsBtn.addEventListener("click", calculateScore)
